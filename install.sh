b2b77101af5500264df58af8da366448b8327fec
Sho Kohara <shokohara@users.noreply.github.com>
2017-06-21 00:41:39 +0900

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/sh -eux
 sudo apt-get update
-sudo apt-get install -y haskell-stack git
+sudo apt-get install -y git
+
+curl -sSL https://get.haskellstack.org/ | sh
+
 git clone --depth=1 https://github.com/shokohara/haskell-gcp-resizedisk.git
 cd haskell-gcp-resizedisk
 stack --install-ghc install

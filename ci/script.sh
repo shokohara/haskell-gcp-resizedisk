311e2133ae1ce27d258d2917f825943c60f72efa
shokohara <shokohara@users.noreply.github.com>
2017-06-21 15:29:51 +0900

    Update script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 export PATH=$HOME/.local/bin:$PATH
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/..
+echo $script_dir
+stack --no-terminal docker pull
 stack --no-terminal --install-ghc --stack-yaml=stack.docker.yaml clean
 stack --no-terminal --install-ghc --stack-yaml=stack.docker.yaml test --no-haddock-deps
 mkdir -p resizedisk_linux_amd64

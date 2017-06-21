51a3d8387c1a7fe2b5580e82ae471eca392782c6
shokohara <shokohara@users.noreply.github.com>
2017-06-21 18:12:19 +0900

    Update App.hs

--- src/App.hs
+++ src/App.hs
@@ -1,33 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module App where
 
 import Control.Lens ((&), (.~), (<&>), (?~), (^.))
 import Network.Google
+import Control.Monad.IO.Class  (MonadIO (..))
+import qualified Network.HTTP.Client as Client
 import GHC.Int
 import Network.Google.Compute
 import Control.Monad.Trans.Resource (runResourceT)
+import Data.Aeson
+import Data.Maybe
 import System.IO (stdout, hGetContents)
 import qualified Network.Google as Google
 import qualified Network.Google.Compute as Compute
 import System.Process
+import GHC.Generics
 import Option
 import qualified Option as O
 import Network.HTTP.Client
 import Network.Google.Compute.Metadata
 import Data.Text (Text)
+import qualified Data.Text as T
+
+data MetaDisk = MetaDisk { deviceName :: Text } deriving (Show, Generic)
+
+instance FromJSON MetaDisk
 
 run :: Option -> IO ()
 run config = storategy (O.percent config) (fromIntegral $ O.gb config) >>= \x ->
   case x of
     Just gbR -> do
-      instanceId <- exampleInstanceId
       projectId <- exampleProjectId
       zoneR <- exampleZone
-      diskNameM <- exampleGetDisks projectId instanceId zoneR
+      diskNameM <- listToMaybe <$> exampleListDisks
+      _ <- print projectId
+      _ <- print zoneR
+      _ <- print diskNameM
       case diskNameM of
-        Just diskName -> do
-          _ <- exampleDiskResizeGCP projectId diskName zoneR gbR
+        Just disks -> do
+          _ <- exampleDiskResizeGCP projectId (deviceName disks) zoneR gbR
           exampleDiskResizeOS
         Nothing -> putStrLn "no Disk"
     Nothing -> return ()
@@ -46,6 +59,19 @@ exampleDiskResizeGCP p d z gbR = do
   lgr <- Google.newLogger Google.Debug stdout
   envR <- Google.newEnv <&> (Google.envLogger .~ lgr) . (Google.envScopes .~ Compute.computeScope)
   runResourceT . Google.runGoogle envR $ Google.send $ disksResize p d z (disksResizeRequest & drrSizeGb ?~ gbR)
+
+getDisk :: MonadIO m => Manager -> m [MetaDisk]
+getDisk m = do
+  rs <- getMetadata "instance/disks/?alt=json&recursive=true" [] m
+  case eitherDecode' (Client.responseBody rs) of
+    Left  _  -> pure []
+    Right xs -> pure xs
+
+--[{"deviceName":"instance-1","index":0,"mode":"READ_WRITE","type":"PERSISTENT"}]
+exampleListDisks :: IO [MetaDisk]
+exampleListDisks = do
+  m <- newManager defaultManagerSettings
+  isGCE m >>= \x -> if x then getDisk m else fail "no GCE"
 
 exampleGetDisks :: Text -> Text -> Text -> IO (Maybe Text)
 exampleGetDisks p d z = do
@@ -68,10 +94,10 @@ used = do
   _ <- waitForProcess ph3
   return $ (read :: String -> Int) $ reverse . drop 2 . reverse $ c -- drop "%\n"
 
-exampleInstanceId :: IO Text
-exampleInstanceId = do
-  m <- newManager defaultManagerSettings
-  isGCE m >>= \x -> if x then getInstanceId m else fail "no GCE"
+-- exampleInstanceId :: IO Text
+-- exampleInstanceId = do
+--   m <- newManager defaultManagerSettings
+--   isGCE m >>= \x -> if x then getInstanceId m else fail "no GCE"
 
 exampleProjectId :: IO Text
 exampleProjectId = do
@@ -81,5 +107,5 @@ exampleProjectId = do
 exampleZone :: IO Text
 exampleZone = do
   m <- newManager defaultManagerSettings
-  isGCE m >>= \x -> if x then getZone m else fail "no GCE"
+  T.reverse . T.takeWhile (\x -> x /= '/') . T.reverse <$> (isGCE m >>= \x -> if x then getZone m else fail "no GCE")
 

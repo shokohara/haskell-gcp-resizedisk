5dd9d9372947c0a9858693c1d3541258206211bc
shokohara <shokohara@users.noreply.github.com>
2017-06-21 16:02:31 +0900

    Update script.bash

--- ci/script.bash
+++ ci/script.bash
@@ -3,8 +3,8 @@ export PATH=$HOME/.local/bin:$PATH
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/..
 echo $script_dir
 stack --no-terminal docker pull
-stack --no-terminal --install-ghc --stack-yaml=stack.docker.yaml clean
-stack --no-terminal --install-ghc --stack-yaml=stack.docker.yaml test --no-haddock-deps
+stack --no-terminal --install-ghc clean
+stack --no-terminal --install-ghc test --no-haddock-deps
 mkdir -p resizedisk_linux_amd64
 cp `stack path --docker --local-install-root`/bin/* $_
 tar -zcvf $_.tar.gz $_
